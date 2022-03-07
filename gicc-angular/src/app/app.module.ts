56e5efbad9d1a25411fe52af1ac6afc2eacf7824
ravikiranmukkarala <ravikiranmukkarala1999@gmail.com>
2022-03-07 15:48:27 +0530

    custom pipe added

--- gicc-angular/src/app/app.module.ts
+++ gicc-angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UiModule } from './ui/ui.module';
-
+import { UsdinrPipe } from './custompipes/usdinr.pipe';
+ 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    UsdinrPipe,
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ gicc-angular/src/app/custompipes/usdinr.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'usdinr'
+})
+export class UsdinrPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
+}
